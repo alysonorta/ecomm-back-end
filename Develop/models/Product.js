3a94b4b41c7997c90f3d7a67fc040ace9d058466
Alyson Orta <alyson.orta@gmail.com>
2021-08-01 14:41:37 -0400

    updated code for product

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -6,7 +6,7 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+//Fields and rules for Product model
 Product.init(
   {
     id: {
