a1c32ce41c1fb4619428f3fcf1edbc5fadc869be
Alyson Orta <alyson.orta@gmail.com>
2021-08-03 21:11:37 -0400

    added comma

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,7 +16,7 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag {
+Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
